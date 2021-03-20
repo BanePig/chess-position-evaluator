aa61c6f7fcd3c301b90ef0a78905002bf9a69218
BudgetToaster <28935064+BanePig@users.noreply.github.com>
2021-03-20 09:30:11 -0400

    Added model.py and fixed db.py

--- db.py
+++ db.py
@@ -4,37 +4,35 @@ import sqlite3
 import h5py
 
 
-def pgn_db_to_hdf5(pgn_db, hdf5_fp):
+def pgn_db_to_hdf5(pgn_fp, hdf5_fp):
     """Creates an hdf5 file with 2 databases: x, and y.
     x is an array of all positions ever reached.
     y is an array of the number of games that included that position, and how many of them white won.
-    For example, if x[0] is a position, then y[0, 0] is how many times that position has been played in a game, and y[0, 1] is how
-    many of those games have been won.
-    The x database has shape: (n, 384)
+    For example, if x[0] is a position, then y[0, 0] is how many times that position has been played in a game,
+    and y[0, 1] is how many of those games have been won.
+
+    The x database has shape: (n, 903); 8 * 8 * (6 + 6 + 2) + 7.
     The y database has shape: (n, 2)"""
-    # TODO: Everything lol
-    pass
 
-
-with open("data/lichess_db_standard_rated_2017-04.pgn", "r") as pgn_file:
-    pgn = ""
-    num_blank_lines = 0
-    index = 0
-    invalid = False
-    for line in pgn_file:
-        if line == "[Termination \"Abandoned\"]\n":
-            invalid = True
-            continue
-        if index > 10:
-            break
-        if line.strip() == "":
-            num_blank_lines += 1
-        if num_blank_lines == 2:
-            if not invalid:
-                print(pgn)
-            invalid = False
-            index += 1
-            pgn = ""  # Keep track of this somehow
-            num_blank_lines = 0
-            continue
-        pgn += line
+    with open(pgn_fp, "r") as pgn_file:
+        pgn = ""
+        num_blank_lines = 0
+        index = 0
+        invalid = False
+        for line in pgn_file:
+            if line == "[Termination \"Abandoned\"]\n":
+                invalid = True
+                continue
+            if index > 10:
+                break
+            if line.strip() == "":
+                num_blank_lines += 1
+            if num_blank_lines == 2:
+                if not invalid:
+                    print(pgn)
+                invalid = False
+                index += 1
+                pgn = ""  # Keep track of this somehow
+                num_blank_lines = 0
+                continue
+            pgn += line
--- model.py
+++ model.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
 
+# INPUTS:
+
 def create_model():
     return tf.keras.models.Sequential([
-        
+        tf.keras.layers.Dense(32768, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(16384, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(8192, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(4096, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(2048, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(1024, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(512, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
+        tf.keras.layers.Dense(3, activation='softmax', kernel_regularizer=tf.keras.regularizers.L2(0.01)),
     ])
